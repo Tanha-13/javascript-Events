e20a69b1a8c91395613ff9a04d67e6c49f531f26
Tanha <nusrattanha897@gmail.com>
2024-05-18 23:25:23 +0600

    events and event listener

---
+++ mdn/mdn-notes.js
@@ -0,0 +1,26 @@
+// Document Object Model -> Interfaces -> Event and EventTarget
+// Topic: Introduction to Events
+/**
+ * Web Events are not part of the core javascript language. They are defined as the part of APIs.
+ * 
+ * Events are things that happen in the system when programming - the system produces or fires a signal of something when an event occurs. Events are fired inside the browser window.
+ * Examples of events - 
+ *  the user chooses a key on the keyboard
+ *  the user resizes or closes the browser window.
+ *  a webpage finishes loading
+ *  a form is submitted
+ *  a video is played, paused, or ends
+ *  an error occurs
+ * 
+ * to react to an event, you can attach an event handler to it. 
+ * When a block of code is defined to run in response to an event, we say we are registering an event handler. 
+ * 
+ * Event handlers and event listeners are pretty much interchangeable. But they are not similar. Event listener listen out for the event happening, and the handler is the code that is run in response to it happening.
+ */
+
+
+
+
+
+
+// Event reference - https://developer.mozilla.org/en-US/docs/Web/Events
